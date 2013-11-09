4ea072e81906b92333a5085fd81418f391f19eff
Juuso Lehtinen <jupelehtinen@gmail.com>
2013-11-10 00:26:58 +0200

    Refactor & styled page

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
+/**
+*   OpenLayers 3 map for EPSG:3067 projection.
+*/
+
+// CONSTRAINTS
+var PROXY_LOCATION = '/cgi-bin/proxy.cgi?url='; // One does not simply cross domain XML
 
 var map;
 
@@ -5,7 +11,8 @@ function init() {
     Proj4js.defs["EPSG:3067"] = "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs";
 
     var projection = ol.proj.configureProj4jsProjection({
-      code: 'EPSG:3067', units: ol.proj.Units.METERS,
+      code: 'EPSG:3067',
+      units: ol.proj.Units.METERS,
       extent: [50199.4814, 6582464.0358, 761274.6247, 7799839.8902]
     });
 
@@ -31,29 +38,20 @@ function init() {
               })
         ],
         view: new ol.View2D({
-          center: [257950.93897922945, 6871557.060772844], zoom: 8, projection: projection
+          center: [257950.93897922945, 6871557.060772844],
+          zoom: 8, 
+          projection: projection
         }),
         renderer: ol.RendererHint.CANVAS
     });
 
     // Some test URLs
-    //var url = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&request=getfeature&typename=lv:nba_mjreki_inspire_alue&maxfeatures=300';
+    var url1 = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&maxfeatures=100&request=getfeature&typename=lv:nba_mjreki_inspire_alue&maxfeatures=300';
+    var url2 = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&maxfeatures=100&request=getfeature&typename=lv:peltolohkot_2013';
+    var url3 = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&maxfeatures=100&request=getfeature&typename=lv:pks_suuralue';
 
-    var url = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&maxfeatures=100&request=getfeature&typename=lv:peltolohkot_2013';
-    //var url = 'http://hip.latuviitta.org/cgi-bin/tinyows?service=wfs&version=1.0.0&maxfeatures=100&request=getfeature&typename=lv:pks_suuralue';
-
-    addLayer(url);
-}
-
-function addLayer(url) {
-    var proxyurl = '/cgi-bin/proxy.cgi?url=' + encodeURIComponent(url);
-    
-    var layer = new ol.layer.Vector({
-      source: new ol.source.Vector({
-        parser: new ol.parser.ogc.GML_v2(),
-        url: proxyurl
-      }),
-      style: new ol.style.Style({
+    // Default style for layers
+    var style = new ol.style.Style({
         symbolizers: [
           new ol.style.Fill({
             color: '#FF1A1A',
@@ -63,8 +61,26 @@ function addLayer(url) {
             color: '#FF1A1A'
           })
         ]
-      })
+      });
+
+    // Add layers to map
+    addLayer(url1, style);
+    addLayer(url2, style);
+    addLayer(url3, style);
+}
+
+function addLayer(url, style) {
+    var proxyurl = PROXY_LOCATION + encodeURIComponent(url);
+    
+    var layer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        parser: new ol.parser.ogc.GML_v2(),
+        url: proxyurl
+      }),
+      style: style
     });
 
     map.addLayer(layer);
+
+    return layer;
 }
